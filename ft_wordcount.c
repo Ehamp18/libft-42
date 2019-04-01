1616ff9eb2bba9758b58ef734dab9a84caeef0ff
Elijah Hampton <elhampto@e1z2r7p16.42.us.org>
2019-03-31 22:02:23 -0700

    auto

--- ft_wordcount.c
+++ ft_wordcount.c
@@ -6,7 +6,7 @@
 /*   By: elhampto <elhampto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 19:25:31 by elhampto          #+#    #+#             */
-/*   Updated: 2019/03/27 14:31:49 by elhampto         ###   ########.fr       */
+/*   Updated: 2019/03/31 21:53:42 by elhampto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,13 @@ size_t				ft_wordcount(char const *s, char c)
 	count = 0;
 	if (!s || !*s)
 		return (0);
-	if (!c)
-		c = '\n';
 	while (s[i])
 	{
-		while (s[i] == ' ' || s[i] == '\t' || s[i] == c)
+		while (s[i] == c)
 			i++;
-		if ((s[i] >= 33 && s[i] <= 126) && s[i] != c)
+		if (s[i] && s[i] != c)
 			count++;
-		while (s[i] >= 33 && s[i] <= 126 && s[i] != c && s[i])
+		while (s[i] != c && s[i])
 			i++;
 	}
 	return (count);
